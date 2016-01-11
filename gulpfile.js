a246a80f91fcd7937a182c12587bec753b465381
masonfox <masonfox22@gmail.com>
2016-01-11 13:23:51 -0500

    added autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 'use strict';
- 
+
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 var plumber = require('gulp-plumber');
-
-gulp.task('serve', ['sass'], function() {
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var clean = require('gulp-clean');
+var jade = require('gulp-jade');
+var autoprefixer = require('gulp-autoprefixer');
+ 
+gulp.task('serve', ['sass', 'jade', 'js'], function() {
 
     browserSync.init({
         server: './',
@@ -14,7 +20,8 @@ gulp.task('serve', ['sass'], function() {
     });
 
     gulp.watch('./scss/**/*.scss', ['sass']);
-    gulp.watch('./*.html').on('change', browserSync.reload);
+    gulp.watch('./**/*.jade', ['jade']);
+    gulp.watch('./js/*.js', ['js-watch']).on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
@@ -22,13 +29,41 @@ gulp.task('sass', function() {
     return gulp.src('./scss/**/*.scss')
         .pipe(plumber())
         .pipe(sass())
+        .pipe(autoprefixer())
         .pipe(gulp.dest('css'))
         .pipe(browserSync.stream());
 });
 
-gulp.task('default', ['serve']);
-
-
-// gulp.task('sass:watch', function () {
-//   gulp.watch('./scss/**/*.scss', ['sass']);
+// gulp.task('autoprefixer', function () {
+// 	return gulp.src('css/style.css')
+// 		.pipe(autoprefixer({
+// 			browsers: ['last 2 versions'],
+// 			cascade: false
+// 		}))
+// 		.pipe(gulp.dest('css/style.css'));
 // });
+
+gulp.task('clean', function () {
+    return gulp.src('js/min', {read: false})
+      .pipe(clean());
+});
+
+gulp.task('js', ['clean'], function(){
+    return gulp.src('./js/*.js')
+        .pipe(concat('concat.js'))
+        .pipe(gulp.dest('js/min'))
+        .pipe(rename('uglify.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('js/min'));
+});
+gulp.task('js-watch', ['js']);
+
+gulp.task('jade', function() {
+  return gulp.src('jade/**/*.jade')
+    .pipe(jade()) // pip to jade plugin
+    .pipe(gulp.dest('./')) // tell gulp our output folder
+    .pipe(browserSync.stream());
+});
+
+// register main task
+gulp.task('default', ['serve']);
